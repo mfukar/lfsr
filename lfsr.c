965cba621a4b61e4c9895138a517fd49960a1981
Michael Foukarakis <mfukar@census-labs.com>
2013-09-18 13:54:35 +0100

    Pass the correct type to sizeof

--- lfsr.c
+++ lfsr.c
@@ -1,26 +1,26 @@
 /*
- * @file	lfsr.c
- * @author	Michael Foukarakis
- * @version	1.1
+ * @file    lfsr.c
+ * @author  Michael Foukarakis
+ * @version 1.1
  * @date
- * 	Created:     Wed Feb 02, 2011 18:47 EET
- * 	Last Update: Tue Mar 26, 2013 12:40 GTB Standard Time
+ *  Created:     Wed Feb 02, 2011 18:47 EET
+ *  Last Update: Wed Sep 18, 2013 13:54 BST
  *------------------------------------------------------------------------
- * Description:	Galois LFSR software implementation
- * 		WARNING:
- * 		Polynomial representation: x^4 + x^3 + 1 = 11001 = 0x19
+ * Description: Galois LFSR software implementation
+ *      WARNING:
+ *      Polynomial representation: x^4 + x^3 + 1 = 11001 = 0x19
  *
  * Well-known polynomials:
- * CRC-12	: x^12 + x^11 + x^3 + x^2 + x + 1
- * CRC-16-IBM	: x^16 + x^15 + x^2 + 1
- * CRC-16-DECT	: x^16 + x^10 + x^8 + x^7 + x^3 + 1
- * CCITT	: x^16 + x^12 + x^5 + 1
- * CRC-32-IEEE	: x^32 + x^26 + x^23 + x^22 + x^16 + x^12 + x^11 + x^10 + x^8 + x^7 + x^5 + x^4 + x^2 + x + 1
+ * CRC-12   : x^12 + x^11 + x^3 + x^2 + x + 1
+ * CRC-16-IBM   : x^16 + x^15 + x^2 + 1
+ * CRC-16-DECT  : x^16 + x^10 + x^8 + x^7 + x^3 + 1
+ * CCITT    : x^16 + x^12 + x^5 + 1
+ * CRC-32-IEEE  : x^32 + x^26 + x^23 + x^22 + x^16 + x^12 + x^11 + x^10 + x^8 + x^7 + x^5 + x^4 + x^2 + x + 1
  *
  * Spread-spectrum sequences:
- * 7-bit	: x^7 + x + 1
- * 13-bit	: x^13 + x^4 + x^3 + 1
- * 19-bit	: x^19 + x^5 + x^2 + x + 1
+ * 7-bit    : x^7 + x + 1
+ * 13-bit   : x^13 + x^4 + x^3 + 1
+ * 19-bit   : x^19 + x^5 + x^2 + x + 1
  *
  * Used in A5:
  * x^19 + x^5 + x^2 + x + 1
@@ -36,8 +36,8 @@
  * http://homepage.mac.com/afj/taplist.html
  * http://www.xilinx.com/support/documentation/application_notes/xapp052.pdf
  *------------------------------------------------------------------------
- * History:	None yet
- * TODO:	Nothing yet
+ * History: None yet
+ * TODO:    Nothing yet
  *------------------------------------------------------------------------
  */
 #include "lfsr.h"
@@ -45,7 +45,7 @@
 void GLFSR_init(lfsr_t *glfsr, lfsr_data_t polynom, lfsr_data_t seed_value)
 {
     lfsr_data_t seed_mask;
-    unsigned int shift = 8 * sizeof(lfsr_data) - 1;
+    unsigned int shift = 8 * sizeof lfsr_data_t - 1;
 
     glfsr->polynomial = polynom | 1;
     glfsr->data = seed_value;
@@ -53,8 +53,8 @@ void GLFSR_init(lfsr_t *glfsr, lfsr_data_t polynom, lfsr_data_t seed_value)
     seed_mask = 1;
     seed_mask <<= shift;
 
-    while(shift--) {
-        if(polynom & seed_mask) {
+    while (shift--) {
+        if (polynom & seed_mask) {
             glfsr->mask = seed_mask;
             break;
         }
@@ -69,7 +69,7 @@ unsigned char GLFSR_next(lfsr_t *glfsr)
 
     glfsr->data <<= 1;
 
-    if(glfsr->data & glfsr->mask) {
+    if (glfsr->data & glfsr->mask) {
         retval = 1;
         glfsr->data ^= glfsr->polynomial;
     }
